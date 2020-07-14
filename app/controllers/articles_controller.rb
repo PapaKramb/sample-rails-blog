cbb1db804f13a6068c2cb20db7c4d277026ba18f
PapaKramb <guccigang5554@gmail.com>
2020-07-15 06:39:08 +0900

    Implement articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,24 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @articles = Article.new(articles_params)
-    if @articles.valid?
-      @articles.save
+    if @articles.save
       redirect_to @articles
     else
       render action: 'new'
+    end
+  end
+
+  def edit
+    @articles = Article.find(params[:id])
+  end
+
+  def update
+    @articles = Article.find(params[:id])
+    
+    if @articles.update(articles_params)
+      redirect_to @articles
+    else
+      render action: 'edit'
     end
   end
 
