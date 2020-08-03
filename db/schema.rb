a133f46133b58156ef32377a028e11712afa4ec7
PapaKramb <guccigang5554@gmail.com>
2020-08-04 01:11:34 +0900

    Add username on sign up

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_144719) do
   end
 
   create_table "terms", force: :cascade do |t|
+    t.text "terms"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,9 +55,10 @@ ActiveRecord::Schema.define(version: 2020_08_03_144719) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"username\"", name: "index_users_on_username", unique: true
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
