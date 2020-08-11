26742aa319e6a4c2fd7f95d5ad17f8f244b52140
PapaKramb <guccigang5554@gmail.com>
2020-08-11 22:23:22 +0900

    Add validation for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy 
-  belongs_to :user, optional: true 
 
   validates :title, presence: true
   validates :text, presence: true
-
-def subject
-title
-end
 end
