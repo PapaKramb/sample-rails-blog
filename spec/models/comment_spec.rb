5d5458d63aa64448be36e93521e41136c142950f
PapaKramb <guccigang5554@gmail.com>
2020-08-17 20:21:34 +0900

    Add validate_length do comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 
 describe Comment do
   it { should belong_to :article }
+  describe "validations" do
+    it { should validate_presence_of  :body }
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
 end
